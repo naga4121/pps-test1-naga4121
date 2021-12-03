8e4f215040bddc4dc6dce2931a7f59627a5bb32c
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2021-12-03 08:44:17 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,10 @@
+#include<stdio.h>
 
+int main(){
+  int num1,num2,res;
+  printf("enter the two numbers:\n");
+  scanf("%d%d",&num1,num2);
+  res=num1+num2;
+  printf("addition of two number is :%d\n",res);
+  return 0
 }
