fdb82d48eed33daa032352c670fe5100b5207517
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2021-12-27 06:30:29 +0000

    Done

---
+++ p5final.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void input(int *a, int *b, int *c)
+{
+ printf("Enter the first number: ");
+ scanf("%d", a);
+ printf("Enter the second number: ");
+ scanf("%d", b);
+ printf("Enter the third number: ");
+ scanf("%d", c);
+}
+int cmp(int *a,int *b,int *c)
+{
+  if(a>b&&a>c)
+  {
+    return *a;
+  }
+  else if(b>c&&b>a)
+  {
+    return *b;
+  }
+  else if(c>a&&c>b)
+  {
+    return *c;
+  }
+}
+void output(int a, int b, int c, int large)
+{
+  printf("The largest of %d,%d,%d is %d\n", a, b, c, large);
+}
+int main()
+{
+  int a,b,c,large;
+  input(&a,&b,&c);
+  large=cmp(&a,&b,&c);
+  output(a,b,c,large);
+  return 0;
+
+}
