3dfdf757dce02ec43239b2f637971abaf4dbc43c
Naga Koushik <94678196+naga4121@users.noreply.github.com>
2022-01-29 11:07:11 +0000

    done

--- p9original.c
+++ p9original.c
@@ -9,7 +9,7 @@ void input_two_string(char *a,char *b)
 int cmp(char *a,char *b)
 {
     int i=0,result;
-    while(a[i] !='\0' && b[i] !='\0')
+    while(https://replit.com/@NagaKoushik1/pps-test1-naga4121-7#p9final.txt[i] !='\0' && b[i] !='\0')
     {
         if(a[i]>b[i])
         {
